5b36d5ec19e0f82bdbd3672247f307c405ca46f6
ivoliveiraa <70412176+ivoliveiraa@users.noreply.github.com>
2020-10-01 19:54:27 -0300

    Update jogo-hora-de-reciclar.py

--- jogo-hora-de-reciclar.py
+++ jogo-hora-de-reciclar.py
@@ -1,13 +1,14 @@
 import pygame
 
+
 pygame.init()
 
-fundo = pygame.image.load('tela pura.png') #fundo da tela
-logo = pygame.image.load('logo2.png') #logo do jogo
-iniciar = pygame.image.load('iniciar.png') #botão iniciar
-resultado = pygame.image.load('ver resultado.png') #botão resultado
-janela = pygame.display.set_mode((1920,1080)) #tamanho da tela
-pygame.display.set_caption("hora de reciclar") # nome que aparece na barra superior da tela
+fundo = pygame.image.load('imagens/tela inicial/tela-inicial.png') #fundo da tela
+logo = pygame.image.load('imagens/tela inicial/logo.png') #logo do jogo
+iniciar = pygame.image.load('imagens/tela inicial/iniciar.png') #botão iniciar
+resultado = pygame.image.load('imagens/tela inicial/ver resultado.png') #botão resultado
+janela = pygame.display.set_mode((1280,720)) #tamanho da tela
+pygame.display.set_caption("imagens/tela inicial/hora de reciclar") #nome que aparece na barra superior da tela
 
 janela_aberta = True
 while janela_aberta:
@@ -17,9 +18,9 @@ while janela_aberta:
 
 
     janela.blit(fundo,(0, 0))
-    janela.blit(logo,(770,100))
-    janela.blit(iniciar,(700,630))
-    janela.blit(resultado,(700,730))
+    janela.blit(logo,(545,100))
+    janela.blit(iniciar,(460,410))
+    janela.blit(resultado,(460,490))
     pygame.display.update()
 
 pygame.quit()
